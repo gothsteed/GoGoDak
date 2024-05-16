9e8eb704eabbeea7d266f7c5684127f5ef42b174
gothsteed <jyleeturbo123@gmail.com>
2024-05-16 15:51:45 +0900

    common controller 추가
    회사소개 => /company.dk
    이용약관 => /agreement.dk
    개인정보처리방침 => /privacy.dk

---
+++ GoGoDak/src/main/java/common/controller/AgreementController.java
@@ -0,0 +1,14 @@
+package common.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AgreementController extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
