c5425e8db3f584c2f2b8dcbdac79f509943ef0e8
kshksh723 <kshksh723@naver.com>
2024-05-18 23:11:54 +0900

    kimsh

--- GoGoDak/src/main/java/member/model/MemberDao.java
+++ GoGoDak/src/main/java/member/model/MemberDao.java
@@ -32,4 +32,10 @@ public interface MemberDao {
 	int getTotalPage(Map<String, String> paraMap)throws SQLException;
 //	■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■혜선작업끝■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 
+	
+	
+	// 입력받은 userid 를 가지고 한명의 회원정보를 리턴시켜주는 메소드
+		MemberVO selectOneMember(String userid) throws SQLException;
+		
+		
 }
