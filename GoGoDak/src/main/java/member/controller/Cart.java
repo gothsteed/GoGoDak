af590a697683f4154bfcccf2bd23bb8739e330e7
yssj4 <yssj4@naver.com>
2024-05-20 17:41:48 +0900

    hansol

--- GoGoDak/src/main/java/member/controller/Cart.java
+++ GoGoDak/src/main/java/member/controller/Cart.java
@@ -1,14 +1,56 @@
 package member.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import common.controller.AbstractController;
+import domain.MemberVO;
+import domain.ProductVO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import member.model.MemberDao;
+import member.model.MemberDao_Imple;
 
 public class Cart extends AbstractController {
 
+	private MemberDao mdao = null;
+
+	public Cart() {
+		mdao = new MemberDao_Imple();
+	}
+	
+	
+	
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
+	HttpSession session =request.getSession();
+		
+		MemberVO loginuser =(MemberVO)session.getAttribute("loginuser");
+		
+		
+		// 사용자가 로그인했는지 확인
+        if (loginuser == null) {
+            super.setRedirect(true);
+            super.setViewPage("/WEB-INF/view/member/member_Login.jsp");
+            return;
+        }
+		
+		 // 세션에서 장바구니 정보 가져오기
+		List<ProductVO> productList = (List<ProductVO>)session.getAttribute("cart");
+        
+        // 장바구니가 없을 경우 빈 장바구니 생성
+        if (productList == null) {
+        	productList = new ArrayList<>();
+        }
+        
+        // 장바구니 정보를 JSP로 전달
+        request.setAttribute("cart", productList);
+     
+        
+    	super.setRedirect(false);
+		super.setViewPage("/WEB-INF/view/member/cart.jsp");
+
 
 	}
 
