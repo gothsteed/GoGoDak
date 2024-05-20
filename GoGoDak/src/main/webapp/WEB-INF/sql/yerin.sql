1769b6aa8ec4b0c2447d7f09c0e5bc5a78613fd7
YERIN JUNG <156673142+yerin9195@users.noreply.github.com>
2024-05-20 14:52:25 +0900

    yerin Member.java update

--- GoGoDak/src/main/webapp/WEB-INF/sql/yerin.sql
+++ GoGoDak/src/main/webapp/WEB-INF/sql/yerin.sql
@@ -22,3 +22,14 @@ select id, name, email,
        case when substr(jubun, 7, 1) in('1', '3') then '남' else '여' end AS GENDER
 from tbl_member
 where id != 'admin'
+
+select *
+from tbl_member
+where id = 'hansol';
+
+delete from tbl_member
+where id = 'hansol';
+-- 1 행 이(가) 삭제되었습니다.
+
+commit;
+-- 커밋 완료.
